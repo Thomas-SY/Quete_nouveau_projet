1b73c0089d2cd6992afce02baaca3c710c4d2c67
Thomas SY <sy.thomas.pro@gmail.com>
2018-11-03 12:36:21 +0100

    Add quest "Doctrine Relation "Many-To-One"".

--- blog/src/Entity/Article.php
+++ blog/src/Entity/Article.php
@@ -61,12 +61,12 @@ class Article
         return $this;
     }
 
-    public function getCategory(): ?category
+    public function getCategory(): ?Category
     {
         return $this->category;
     }
 
-    public function setCategory(?category $category): self
+    public function setCategory(?Category $category): self
     {
         $this->category = $category;
 
