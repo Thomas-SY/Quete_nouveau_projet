243b6be5f57f985e17399362be0df78c8a9dcd84
Thomas SY <sy.thomas.pro@gmail.com>
2018-11-12 10:25:41 +0100

    Add quest "Doctrine relations bidirectionnelle".

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -2,18 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    /**
-     * @Route("/blog/{slug<^$|\w(-*\w)*$>}", defaults={"slug": "Article Sans Titre"}, name="show")
-     */
-    public function show($slug)
+//    /**
+//     * @Route("/blog/{slug<^$|\w(-*\w)*$>}", defaults={"slug": "Article Sans Titre"}, name="show")
+//     */
+    /*public function show($slug)
     {
         $input = str_replace("-"," ",$slug);
         $showTitre = ucwords($input);
         return $this->render('blog/index.html.twig', ['showTitre' => $showTitre]);
+    }*/
+
+    /**
+     * @Route("/", name="article_index")
+     */
+    public function index()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $articlesManager = $entityManager->getRepository(Article::class);
+
+        $articles = $articlesManager->findAll();
+
+        return $this->render('blog/index.html.twig', ['articles' => $articles,]);
     }
+
 }
--- blog/src/Entity/Article.php
+++ blog/src/Entity/Article.php
@@ -27,15 +27,10 @@ class Article
     private $content;
 
     /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="articles")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getTitle(): ?string
     {
@@ -72,4 +67,5 @@ class Article
 
         return $this;
     }
+
 }
