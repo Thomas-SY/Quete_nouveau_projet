72ef683d51fc03d39dce726a9d9f221c3daa80bf
Thomas SY <sy.thomas.pro@gmail.com>
2018-11-27 09:31:51 +0100

    Add quest "Générer un CRUD”.

---
+++ blog/src/Form/ArticleType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title',TextType::class)
+            ->add('content',TextType::class)
+            ->add('category',EntityType::class, [
+                'required' => true,
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => false ])
+            ->add('tags', EntityType::class, [
+                'required' => false,
+                'class' => Tag::class,
+                'choice_label' => 'name',
+                'multiple' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
